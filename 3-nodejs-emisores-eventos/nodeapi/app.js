e1414429f931078a8c9c4349f124401828be5cb6
Alfredo Sánchez <alfred3ks@gmail.com>
2022-12-10 14:51:08 +0100

    Termino el video de la sesion 3. Express, express validator.

--- 3-nodejs-emisores-eventos/nodeapi/app.js
+++ 3-nodejs-emisores-eventos/nodeapi/app.js
@@ -8,41 +8,74 @@ var logger = require('morgan');
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
+// Se crea un objeto app con la libreria express():
+// Vemos lo metodos:
 var app = express();
 
-// view engine setup
+// view engine setup: Establecemos las vistas de la aplicacion:
+// __dirname -> la carpeta actual: Sobre todo para rutas dinamicas, rutas absolutas:
+console.log(__dirname);
 app.set('views', path.join(__dirname, 'views'));
+// Aqui tenemos el motor de vistas ejs:
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+// Middlewares: Una app de express es una lista de Middlewares, cuando llegue una peticion usa estos Middlewares: Un Middleware es una funcion que recibe tres parametros:
+app.use(logger('dev')); // Cargamos lo log de las peticiones, gracias a Morgan, lo vemos en la consola:, el 'dev' es el formato del log. Lo vemos en la documentacion de morgan.
+
+// Estos los vemos mas adelante:
 app.use(express.json());
+
+//  este middeware da soporte para las peticiones de tipo post o put sean urlencoded, OJO POSTMAN body: parsea el body en este formato si quisieramos otro para parsear en formato form-data podemos usar una libreria llamada multer, tenemos que crera un nuevo middeware.
+// https://www.npmjs.com/package/multer
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// sirve para servir ficheros estaticos:
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/pdf', express.static(path.join(__dirname, 'public/pdf')));
+
+// Creamos nuestros Middeware: Recibe 3 parametros, necesarios req,res,next
+app.use((req, res, next) => {
+  // Hace una de 2 cosas: Pero nunca tener ambas respuestas:
+  // - responder
+  // res.send('ok');
+  // - o llamar a next
+  // console.log('Peticion a ->', req.originalUrl); // Vemos salida por consola:
+  // next(new Error('cosa mala')); // Vemos como es pasarle cosas a next().
+  // Si nos da error por consola: Cannot set headers after they are sent to the client, es que hemos respondido dos veces y eso esta prohibido en el protocolo HTTP. Osea no podemos tener un sen() y luego el next() juntos. O dos Middeware que respondan cada uno una cosa.
+  next();
+
+  // Cuando ponemos next() sin pasarle nada pasa a evaluar el siguiente Middeware, pero si le pasamos un objeto se salta todos y pasa a evaluar el Middeware de errores y se evalua ahi.
+});
 
 // AQUI YA NO METEMOS LAS VARIABLES SINO EL REQUIRE DIRECTO:
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 /**
- * Rutas de mi aplicacion web: MODIFICADAS LA ORIGINA SON LAS RUTAS DE ARRIBA:
- * Cuando hacemos peticiones estas son las rutas que se muestran:
+ * Rutas de mi aplicacion web: ROUTES: Las vemos en la carpeta routes:
+ * MODIFICADAS LA ORIGINA SON LAS RUTAS DE ARRIBA:
+ * Cuando hacemos peticiones estas son las rutas que se muestran: Son Middeware: Son agrupaciones de Middeware
  */
 app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+// Lo comentamos porque no lo vamos a usar, este por lo menos.
+// app.use('/users', require('./routes/users'));
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler. Middeware de errores:
 app.use(function (req, res, next) {
+  // Next llama a una funcion de error: Cargada de la libreria http-error
   next(createError(404));
 });
 
-// error handler
+// error handler. Middeware de gestion de errores, tiene 4 parametros: Este se lanza cuando next llama a un error:
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
+  // res.local para pasar cosas a las vistas:
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
+  // Renderizamos la vista de errores, con el metodo .render() de express.
   res.render('error');
 });
 
--- 3-nodejs-emisores-eventos/nodeapi/routes/index.js
+++ 3-nodejs-emisores-eventos/nodeapi/routes/index.js
@@ -1,9 +1,86 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+// Cargamos el modulo de express validator: object destructuring, solo usaremos esos dos:
+// query -> es un middeware, lo cargamos en donde tengamos peticiones query:
+const { query, validationResult, body, param } = require('express-validator');
+
+/* GET home page.
+Esto tambien es un Middeware.
+('/') -> esto es un filtro para express.
+GET tipo de peticion.
+*/
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// Creamos nuestro Middeware, una ruta nueva:
+// http://localhost:3000/paramenruta/33
+router.get('/paramenruta/:id', (req, res, next) => {
+  // Vemos por el log:
+  console.log('req.params', req.params);
+  res.send('Resp OK solo id');
+})
+
+// http://localhost:3000/params/23/piso/2/puerta/C
+router.get('/params/:id/piso/:piso/puerta/:puerta', (req, res, next) => {
+  // Vemos por el log:
+  console.log('req.params', req.params);
+  res.send('OK-YEAH-toda la ruta');
+})
+
+// http://localhost:3000/params/piso/2/puerta/C Un parametro opcional: ?
+router.get('/params/:id?/piso/:piso/puerta/:puerta', (req, res, next) => {
+  // Vemos por el log:
+  console.log('req.params', req.params);
+  // Los parametros son string, lo vemos en el log:
+  res.send('OK-YEAH-opcional');
+})
+
+// http://localhost:3000/params/23/piso/2/puerta/C Ponemos un filtro por expresion regular id() OJO para ver que este funciona comenta los anteriores.
+// Para expresiones regulares: Ver -> regexr.com
+router.get('/params/:id([0-9]+)/piso/:piso/puerta/:puerta', (req, res, next) => {
+  // Vemos por el log:
+  console.log('req.params', req.params);
+  res.send('OK - expresion- regular');
+})
+
+// Ahora aqui vemos las peticiones con query strings:
+// http://localhost:3000/enquerystring?color=rojo&talla=l&lang=it
+// Probamos url mal y vemos la validacion:
+// http://localhost:3000/enquerystring?color=Rojo&talla=l&lang=it
+
+// Tambien cargamos middeware query:
+router.get('/enquerystring',
+  // Vemos la validacion con middeware query:
+  query('color').isLowercase().withMessage('must be lower case'),
+  // query('talla').isNumeric().withMessage('must be numeric')
+  (req, res, next) => {
+    // Aqui usamos el otro middeware validationResult:
+    validationResult(req).throw(); // Lanza excepcion si no valida lo de query().
+    // Vemos por el log:
+    console.log('req.query', req.query);
+    res.send('OK - query string');
+  })
+
+/*
+Peticiones de tipo POST:
+Para probarlo debemos hacerlo con postman: Es un entorno para el desarrollo de apis.
+http://localhost:3000/rutapost
+
+Pasamos por el body:
+Y los parametros
+key:
+value:
+Y vemos como lo recibimos en el log: con el objeto req.body:
+*/
+router.post('/rutapost', (req, res, next) => {
+  // Vemos por el log:
+  // Recuerda en postman hacer peticion de tipo urlencoded y en el body meter los datos para ver salida en el log:
+  // ver imagen anexa post-1 en practica/img
+  console.log('req.body', req.body);
+  res.send('OK - petición de tipo POST');
+})
+
 
 module.exports = router;
